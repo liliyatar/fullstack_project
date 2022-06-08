338f5642258c8b924c36ecaeb8822bfcb2efd93e
Liliia Kobzeva <liliia.taran@dev.pro>
2022-06-08 14:48:11 +0700

    Add server and client

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
-
+const keys = require('./config/keys');
 const app = express();
 
+mongoose.connect(
+    keys.mongoURI, {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+}).then(() => console.log('Mongo DB connected'))
+.catch(error => console.log(error));
+
+app.use(passport.initialize());
+require('./middleware/passport')(passport);
+
 app.use(require('morgan')('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(require('cors')());
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://liliia:25022000Lola@cluster0.33nzd.mongodb.net/?retryWrites=true&w=majority',
+    jwt: 'dev-jwt'
+}
