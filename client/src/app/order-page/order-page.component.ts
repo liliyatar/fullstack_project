df46ce96451130acb031be2e0f786e92e5623ae6
Liliia Kobzeva <liliia.taran@dev.pro>
2022-07-09 15:23:54 +0700

    changes

--- client/src/app/order-page/order-page.component.ts
+++ client/src/app/order-page/order-page.component.ts
@@ -1,15 +1,83 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { MaterialInstance, MaterialService } from '../shared/classes/material.service';
+import { Order, OrderPosition } from '../shared/interfaces';
+import { OrdersService } from '../shared/services/orders.service';
+import { OrderService } from './order.service';
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
-  styleUrls: ['./order-page.component.css']
+  styleUrls: ['./order-page.component.css'],
+  providers: [OrderService]
 })
-export class OrderPageComponent implements OnInit {
+export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('modal') modalRef: ElementRef;
 
-  constructor() { }
+  public isRoot: boolean;
+  public pending: boolean = false;
+  public modal: MaterialInstance;
+  public oSub: Subscription;
+
+  constructor(
+    private router: Router,
+    public order: OrderService,
+    private ordersService: OrdersService,
+  ) { }
 
   ngOnInit(): void {
+    this.isRoot = this.router.url === '/order';
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isRoot = this.router.url === '/order';
+      }
+    });
   }
 
+  ngOnDestroy(): void {
+    this.modal.destroy();
+    if (this.oSub) {
+      this.oSub.unsubscribe();
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.modal = MaterialService.initModal(this.modalRef);
+  }
+
+  open() {
+    this.modal.open();  
+  }
+
+  cancel() {
+    this.modal.close();
+  }
+
+  submit() {
+    this.pending = true;
+
+    const order: Order = {
+      list: this.order.list.map(item => {
+        delete item._id;
+        return item;
+      })
+    };
+  
+    this.oSub = this.ordersService.create(order).subscribe({
+      next: (newOrder) => {
+        MaterialService.toast(`Заказ №${newOrder.order} успешно добавлен.`);
+        this.order.clear();
+      },
+      error: (error) => MaterialService.toast(error.error.message),
+      complete: () => {
+        this.modal.close();
+        this.pending = false;
+      },
+    });
+  }
+
+  removePosition(orderPosition: OrderPosition) {
+    this.order.remove(orderPosition);
+  }
 }
---
+++ client/src/app/order-page/order-positions/order-positions.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
+import { MaterialService } from 'src/app/shared/classes/material.service';
+import { Position } from 'src/app/shared/interfaces';
+import { PositionsService } from 'src/app/shared/services/positions.service';
+import { OrderService } from '../order.service';
+
+@Component({
+  selector: 'app-order-positions',
+  templateUrl: './order-positions.component.html',
+  styleUrls: ['./order-positions.component.css']
+})
+export class OrderPositionsComponent implements OnInit {
+  public positions$: Observable<Position[]>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private positionsService: PositionsService,
+    private orderService: OrderService,
+  ) { }
+
+  ngOnInit(): void {
+    this.positions$ = this.route.params
+      .pipe(
+        switchMap(
+          (params: Params) => {
+            return this.positionsService.fetch(params['id']);
+          }
+        ),
+        map(
+          (positions: Position[]) => {
+            return positions.map((position) => {
+              position.quantity = 1;
+              return position;
+            });
+          }
+        ),
+      );
+  }
+
+  addToOrder(position: Position) {
+    MaterialService.toast(`Успешно добавлено x${position.quantity}`);
+    this.orderService.add(position);
+  }
+
+}
