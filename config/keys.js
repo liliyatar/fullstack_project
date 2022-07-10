1d7a271a74a53431fa42cfe927a04411df351feb
Liliia Kobzeva <liliia.taran@dev.pro>
2022-07-10 22:25:15 +0700

    changes

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://liliia:25022000Lola@cluster0.33nzd.mongodb.net/?retryWrites=true&w=majority',
-    jwt: 'dev-jwt'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys.prod');
+} else {
+    module.exports = require('./keys.dev');
 }
